fe483c16adef76554656bc1a8d45c00f4a545733
CodeWithDjan <85175356+akromnassir@users.noreply.github.com>
2025-10-21 11:15:41 +0500

    Add files via upload

---
+++ backpart/utils/firebase/enums.js
@@ -0,0 +1,16 @@
+const Role = {
+    ASSISTANT: "assistant",
+    APPROVER: "approver",
+    ADMIN: "admin",
+};
+
+const FileStatus = {
+    APPROVED: "approved",
+    REJECTED: "rejected",
+    CORRECTION: "correction",
+    PENDING: "pending",
+};
+module.exports = {
+    Role,
+    FileStatus,
+};
